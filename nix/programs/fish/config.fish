a88f0c980eff40bd60c04ff616d4af79ba103f11
parsifa1 <li.aldric@gmail.com>
2024-03-21 07:26:21 +0000

    fix:close direnv log

--- nix/programs/fish/config.fish
+++ nix/programs/fish/config.fish
@@ -8,6 +8,7 @@ export FZF_DEFAULT_COMMAND="fd -H -I --exclude={.git,.idea,.vscode,.sass-cache,n
 export FZF_DEFAULT_OPTS="--height 40% --layout=reverse --color=bg+:,bg:,gutter:-1,spinner:#f5e0dc,hl:#f38ba8 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
 export EDITOR="nvim"
 export DISPLAY=":0"
+export DIRENV_LOG_FORMAT=    # close direnv log
 export FLAKE="/home/parsifa1/nixos/"
 export WAYLAND_DISPLAY=wayland-0
 export PATH="$HOME/.cargo/bin:$PATH"
