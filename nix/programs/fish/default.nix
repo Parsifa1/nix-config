c0b19a7613568d7d024409c0b3a62a249e16a7ad
parsifa1 <li.aldric@gmail.com>
2024-03-24 18:32:51 +0000

    update: update

--- nix/programs/fish/default.nix
+++ nix/programs/fish/default.nix
@@ -1,4 +1,4 @@
-{pkgs, ...}: {
+{...}: {
   home.sessionVariables = {
     FZF_DEFAULT_COMMAND = "fd -H -I --exclude={.git,.idea,.vscode,.sass-cache,node_modules,build,.vscode-server,.virtualenvs} --type f";
     FZF_DEFAULT_OPTS = "--height 40% --layout=reverse --color=bg+:,bg:,gutter:-1,spinner:#f5e0dc,hl:#f38ba8 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8";
