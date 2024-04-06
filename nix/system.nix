3a64f3e31828c3de24de5b29e20d8aa8ff9278e1
Parsifa1 <li.aldric@gmail.com>
2024-04-06 12:34:48 +0000

    update: flake

--- nix/system.nix
+++ nix/system.nix
@@ -13,7 +13,11 @@
 
   nix.settings = {
     trusted-users = ["parsifa1"];
-    substituters = ["https://cache.nixos.org" "https://nix-community.cachix.org"];
+    substituters = [
+      "https://mirrors.ustc.edu.cn/nix-channels/store"
+      "https://cache.nixos.org"
+      "https://nix-community.cachix.org"
+    ];
     trusted-public-keys = ["nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
   };
 
