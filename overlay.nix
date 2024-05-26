6d71cc6005359bb11f7d3c32747013c99983264c
Parsifa1 <li.aldric@gmail.com>
2024-05-26 16:55:48 +0000

    fix: spaceline

--- overlay.nix
+++ overlay.nix
@@ -2,10 +2,13 @@
   overlays = with inputs; [
     # my nur overlays
     cloudtide.overlay
+
     #nur overlay
     nur.overlay
+
     # neovim
     # neovim-nightly.overlays.default
+
     #unstable overlays
     (_final: prev: {
       unstable = import nixpkgs {
