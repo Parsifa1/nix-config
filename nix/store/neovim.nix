d00545bdbd4ad5638b35b06f532aa69d15d237ec
Parsifa1 <li.aldric@gmail.com>
2024-07-22 17:00:04 +0000

    feat: delete sqlite

--- nix/store/neovim.nix
+++ nix/store/neovim.nix
@@ -3,8 +3,9 @@
     enable = true;
     package = pkgs.neovim;
   };
-  home = {
-    packages = with pkgs; [lua51Packages.lua luajitPackages.luarocks sqlite.out tree-sitter];
-    # sessionVariables = {LIBSQLITE = "${pkgs.sqlite.out}/lib/libsqlite3.so";};
-  };
+  home.packages = with pkgs; [
+    lua51Packages.lua
+    luajitPackages.luarocks
+    tree-sitter
+  ];
 }
