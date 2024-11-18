a570314aa3cd700aaa75d027752fa7fbc5823918
Parsifa1 <li.aldric@gmail.com>
2024-11-19 03:39:11 +0800

    fix: cc to clang

--- darwin/store/fish.nix
+++ darwin/store/fish.nix
@@ -1,21 +1,22 @@
-{ lib, pkgs, ... }:
+{ ... }:
 let
   EDITOR = "nvim";
+  PATH = "$PATH:${PNPM_HOME}";
   PNPM_HOME = "/Users/parsifa1/.local/share/pnpm";
-  LIBRARY_PATH = lib.makeLibraryPath [ pkgs.libiconv ]; # for neovim blink.cmp lib
+  CARGO_TARGET_AARCH64_APPLE_DARWIN_LINKER = "clang";
   FZF_DEFAULT_COMMAND = "fd -H -I -E '{.astro,.git,.kube,.idea,.vscode,.sass-cache,node_modules,build,.vscode-server,.virtualenvs,target}' --type f --strip-cwd-prefix";
   FZF_DEFAULT_OPTS = "--height 40% --layout=reverse --color=bg+:,bg:,gutter:-1,spinner:#f5e0dc,hl:#f38ba8 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8";
 in
 {
   home.sessionVariables = {
     inherit
+      PATH
       EDITOR
       PNPM_HOME
-      LIBRARY_PATH
-      FZF_DEFAULT_COMMAND
       FZF_DEFAULT_OPTS
+      FZF_DEFAULT_COMMAND
+      CARGO_TARGET_AARCH64_APPLE_DARWIN_LINKER
       ;
-    PATH = "$PATH:${PNPM_HOME}";
   };
   programs.fish = {
     enable = true;
@@ -26,14 +27,14 @@ in
     shellAliases = {
       v = "nvim";
       vi = "nvim";
-      lg = "lazygit";
-      fa = "fastfetch";
       cls = "clear";
       py = "python";
-      zf = "z \$(fd --type d --hidden . 2>/dev/null | fzf)";
+      lg = "lazygit";
+      fa = "fastfetch";
+      zf = "z \$(fd --type d --hidden . 3>/dev/null | fzf)";
       ghostty = "/Applications/Ghostty.app/Contents/MacOS/ghostty";
       ls = "eza --icons --group-directories-first --sort=extension -F";
-      vf = "set -l file (fzf --preview 'bat --style=numbers --color=always --line-range :500 {}'); and test -n \"\$file\"; and vi \"\$file\"";
+      vf = "set -l file (fzf --preview 'bat --style=numbers --color=always --line-range :501 {}'); and test -n \"\$file\"; and vi \"\$file\"";
     };
   };
 }
