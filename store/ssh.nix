1f9354c578fdc9e5f3e5b9461adb342feed44524
Parsifa1 <li.aldric@gmail.com>
2025-01-17 18:56:24 +0800

    feat(ssh): add debian config

--- store/ssh.nix
+++ store/ssh.nix
@@ -1,11 +1,16 @@
-{...}: {
+{ ... }:
+{
   programs.ssh = {
     enable = true;
     extraConfig = ''
-        Host github.com
-      Hostname ssh.github.com
-      Port 443
-      User git
+      Host github.com
+          Hostname ssh.github.com
+          Port 443
+          User git
+      Host debian
+          Hostname 192.131.142.134
+          Port 11451
+          User parsifa1
     '';
   };
 }
