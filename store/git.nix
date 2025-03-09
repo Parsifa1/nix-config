9df7b71f667f2195c3149ccf25b67a1057795a88
Parsifa1 <li.aldric@gmail.com>
2025-03-09 09:35:54 +0000

    fix(git): remove sign for debian

--- store/git.nix
+++ store/git.nix
@@ -7,16 +7,13 @@ in
     enable = true;
     userName = "Parsifa1";
     userEmail = "li.aldric@gmail.com";
-    #FIX: gitFull have error in nixpkgs
-    # ref: https://github.com/NixOS/nixpkgs/issues/369143
-    package = pkgs.git;
-    # package = pkgs.gitFull;
+    package = pkgs.gitFull;
     extraConfig = {
       user.signingkey = "99B21766F86301CA";
       credential.helper = if stdenv.isDarwin then "osxkeychain" else "store";
       core.editor = "nvim";
       credential.credentialStore = "cache";
-      commit.gpgsign = true;
+      commit.gpgsign = if builtins.getEnv "hostname" == "debian" then false else true;
     };
   };
 }
