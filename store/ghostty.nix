865d23e5af966a832580d9eefcd07389960061e5
Parsifa1 <li.aldric@gmail.com>
2025-08-02 17:49:44 +0800

    fix(ghostty): fix command+q to alt+q

--- store/ghostty.nix
+++ store/ghostty.nix
@@ -15,11 +15,11 @@ let
         macos-titlebar-proxy-icon = "visible";
         macos-icon-screen-color = "blue";
         keybind = [
-          "command+q=unbind"
           "super+enter=unbind"
           "option+left=unbind"
           "option+right=unbind"
           "ctrl+v=paste_from_clipboard"
+          "command+q=text:\\x1b[113;3u" # send Alt + q with escape sequence
           "ctrl+shift+apostrophe=new_split:right"
           "ctrl+shift+semicolon=new_split:down"
         ];
@@ -32,10 +32,10 @@ let
         window-height = 42;
         window-width = 170;
         keybind = [
+          "ctrl+enter=unbind"
           "ctrl+v=paste_from_clipboard"
           "ctrl+shift+apostrophe=new_split:right"
           "ctrl+shift+semicolon=new_split:down"
-          "ctrl+enter=unbind"
         ];
       };
   settings = special // {
@@ -43,45 +43,42 @@ let
       "IosevkaCloudtide Nerd Font"
       "LXGW WenKai"
     ];
+    cursor-style = "block";
+    confirm-close-surface = false;
+    shell-integration-features = "no-cursor";
     window-title-font-family = "Iosevka Cloudtide";
     theme = "dark:Everforest,light:GruvboxLightHard";
-    # background = "#272E33";
-    cursor-style = "block";
-    shell-integration-features = "no-cursor";
-    confirm-close-surface = false;
   };
 in
 {
   programs.ghostty = {
     enable = true;
     package = if isDarwin then null else pkgs.ghostty;
-    themes = {
-      Everforest = {
-        background = "#272E33";
-        foreground = "#d3c6aa";
-        cursor-color = "#e69875";
-        cursor-text = "#4c3743";
-        palette = [
-          "0=#7a8478"
-          "1=#e67e80"
-          "2=#a7c080"
-          "3=#dbbc7f"
-          "4=#7fbbb3"
-          "5=#d699b6"
-          "6=#83c092"
-          "7=#f2efdf"
-          "8=#a6b0a0"
-          "9=#f85552"
-          "10=#8da101"
-          "11=#dfa000"
-          "12=#3a94c5"
-          "13=#df69ba"
-          "14=#35a77c"
-          "15=#fffbef"
-        ];
-        selection-background = "#4c3743";
-        selection-foreground = "#d3c6aa";
-      };
+    themes.Everforest = {
+      background = "#272E33";
+      foreground = "#d3c6aa";
+      cursor-color = "#e69875";
+      cursor-text = "#4c3743";
+      palette = [
+        "0=#7a8478"
+        "1=#e67e80"
+        "2=#a7c080"
+        "3=#dbbc7f"
+        "4=#7fbbb3"
+        "5=#d699b6"
+        "6=#83c092"
+        "7=#f2efdf"
+        "8=#a6b0a0"
+        "9=#f85552"
+        "10=#8da101"
+        "11=#dfa000"
+        "12=#3a94c5"
+        "13=#df69ba"
+        "14=#35a77c"
+        "15=#fffbef"
+      ];
+      selection-background = "#4c3743";
+      selection-foreground = "#d3c6aa";
     };
     enableFishIntegration = true;
     inherit settings;
