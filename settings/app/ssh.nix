d6af1b187a54c20a8e8d0d1dc8e21c25b6e52e0b
Parsifa1 <li.aldric@gmail.com>
2025-08-30 16:33:23 +0800

    fix(home-manager): add default ssh * block
    ref: https://github.com/nix-community/home-manager/pull/7737

--- settings/app/ssh.nix
+++ settings/app/ssh.nix
@@ -2,6 +2,20 @@
 {
   programs.ssh = {
     enable = true;
+    enableDefaultConfig = false;
+    # default ssh config
+    matchBlocks."*" = {
+      forwardAgent = false;
+      addKeysToAgent = "no";
+      compression = false;
+      serverAliveInterval = 0;
+      serverAliveCountMax = 3;
+      hashKnownHosts = false;
+      userKnownHostsFile = "~/.ssh/known_hosts";
+      controlMaster = "no";
+      controlPath = "~/.ssh/master-%r@%n:%p";
+      controlPersist = "no";
+    };
     includes = [
       "includes/${config.age.secrets.ssh-config.name}"
     ];
